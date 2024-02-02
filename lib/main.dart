03983d79fbebbb15912a013de35311518a94391b
Sathia Pang <pangvichetsatya1@gmail.com>
2024-02-02 13:52:46 +0700

    Update on main file

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
 
         // home: const SplashScreen());
         home: SearchScreen());
-        home: const SplashScreen());
+        home: const SplashScreen();
     // home: ForgotPasswordScreen());
   }
 }
