dc99a76a68801f616bf606e1649e7d8424e729b5
Panha Keat <keatpanha2@gmail.com>
2024-02-05 13:11:00 +0700

    update navigator

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
-        home: NavigatorScreen());
+        // home: NavigatorScreen());
+        home: const SplashScreen());
   }
 }
