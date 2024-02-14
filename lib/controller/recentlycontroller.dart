d475bfbc6b185c8ee901b76636a63e08a97edccf
PPC <ppc@PPCs-MacBook-Pro.local>
2024-02-14 16:08:32 +0700

    Done SeeAll Screen

--- lib/controller/recentlycontroller.dart
+++ lib/controller/recentlycontroller.dart
@@ -1,24 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hotel/constant.dart';
 
 class RecentlyController extends GetxController {
   final RxBool isGridVisible = false.obs;
-  final Rx<Color> iconColor = Colors.green.obs;
 
   void toggleView() {
     isGridVisible.value = false;
     update();
-    updateColor();
   }
 
   void showGridView() {
     isGridVisible.value = true;
     update();
-    updateColor();
-  }
-
-  void updateColor() {
-    iconColor.value = isGridVisible.value ? Colors.black : Colors.green;
   }
 }
