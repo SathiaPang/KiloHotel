9219737b9f602a4fb5fe21c947e89fb7b2039b1c
So LayZz <ppc@PPCs-MacBook-Pro-2.local>
2024-03-05 15:45:43 +0700

    Done API Login

--- lib/view/satya/Search screen/demoSearch.dart
+++ lib/view/satya/Search screen/demoSearch.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hotel/constant/constant.dart';
-
 import 'package:hotel/data/data.dart';
 
 class DemoSearch extends StatelessWidget {
@@ -14,13 +13,94 @@ class DemoSearch extends StatelessWidget {
       () => ListView.builder(
         itemCount: hotelList.length,
         itemBuilder: (context, index) {
-          //final data = hotelList[index];
+          final data = hotelList[index];
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              height: Get.height * 0.1,
-              width: Get.width * 0.5,
-              color: green,
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: greenAccent,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: AssetImage(data.img),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '${data.name}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '${data.location}',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              Text(
+                                '5.0',
+                                style: TextStyle(
+                                    fontSize: 17, color: Colors.green),
+                              ),
+                              SizedBox(width: 5),
+                              Text("(4,345 reviews)"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "\$ ${data.price}",
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text("/night"),
+                        // Obx(
+                        //   () => IconButton(
+                        //     onPressed: () {
+                        //       iconColor.value = iconColor.value == Colors.black
+                        //           ? Colors.green
+                        //           : Colors.black;
+                        //     },
+                        //     icon: Icon(Icons.bookmark),
+                        //     color: iconColor.value,
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           );
         },
